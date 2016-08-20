018b846f4dc1c39009b18a1b7ac251f288818285
OldBigBuddha <shinlag@outlook.jp>
2016-08-20 22:45:38 +0900

    [OK]

--- app/src/main/java/com/inhomeubuntu/oji/calcuranai/MainActivity.java
+++ app/src/main/java/com/inhomeubuntu/oji/calcuranai/MainActivity.java
@@ -3,11 +3,11 @@ package com.inhomeubuntu.oji.calcuranai;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -74,23 +74,49 @@ public class MainActivity extends AppCompatActivity {
                 textResult.setTextColor(Color.parseColor("#DD8B91"));
                 textResult.setText(nameTotal_long + "%");
 
+                isUranai = true;
+
                 linearLayout.requestFocus();
                 resultButton.setText("もう一回!");
 
                 name1_Edit.setText("");
                 name2_Edit.setText("");
             } else if (isUranai) {
+                name1_Edit.requestFocus();
                 textResult.setText(result_Str);
                 resultButton.setText("占う！");
+                isUranai = false;
             }
         }else if (name1_Str == null) {
-            Snackbar.make(linearLayout,"一人目の名前を入れてないよ", Snackbar.LENGTH_SHORT).show();
+//            name1_Edit.requestFocus();
+//            Snackbar.make(linearLayout,"一人目の名前を入れてないよ", Snackbar.LENGTH_SHORT).show();
         }else if (name2_Str == null) {
-            Snackbar.make(linearLayout,"ニ人目の名前を入れてないよ", Snackbar.LENGTH_SHORT).show();
+//            name2_Edit.requestFocus();
+//            Snackbar.make(linearLayout,"ニ人目の名前を入れてないよ", Snackbar.LENGTH_SHORT).show();
         }else {
-            Snackbar.make(linearLayout,"どっちも入力してないよ",Snackbar.LENGTH_SHORT).show();
+//            name1_Edit.requestFocus();
+//            Snackbar.make(linearLayout,"どっちも入力してないよ",Snackbar.LENGTH_SHORT).show();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        final int ID = item.getItemId();
+
+        if (ID == R.id.actionFinish) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public double convert(String name) {
